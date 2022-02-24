1a63f08555867ae0cdae024bd6990cec9a50afb0
unknown <1215186630@qq.com>
2022-02-24 15:23:55 +0800

    commit my project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <footer-guide v-show="$route.meta.showFooter"/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide.vue';
+export default {
+  name: 'App',
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    .app
+     width 100%
+     height 100%
+     background #f5f5f5
+</style>
